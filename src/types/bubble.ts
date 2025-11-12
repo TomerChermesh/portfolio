b51fa316ed3fd449459a6747edcba47fac5c8543
TomerChermesh <tomerchermesh@gmail.com>
2025-11-12 15:51:43 +0200

    fix bubbles

--- src/types/bubble.ts
+++ src/types/bubble.ts
@@ -1,6 +1,8 @@
+import React from "react";
+
 export type BubbleConfig = {
   id: string;
-  icon: string;
+  icon: React.ReactNode;
   label?: string;
   subLabel?: string;
   onClick?: () => void;
